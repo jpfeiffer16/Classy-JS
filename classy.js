0296f86a4900040bd4f69f44d701086dbc78d398
jpfeiffer16 <jpfeiffer@bluemodus.com>
2015-08-12 19:42:30 -0600

    Changes

--- classy.js
+++ classy.js
@@ -31,7 +31,14 @@ Array.prototype.accessify = function() {
 	var returnObject = {};
 	return function returnFunction() {
 		for (var i = 0; i < methodGroups.length; i++) {
-			returnFunction[methodGroups[i][1].name] = methodGroups[i][1];
+			if (typeof(methodGroups[i][1]) == 'function') {
+				returnFunction[methodGroups[i][1].name] = methodGroups[i][1];
+			} else if (typeof(methodGroups[i][1]) == 'object') {
+				for (var property in methodGroups[i][1]) {
+					returnFunction[property] = methodGroups[i][1][property];
+				};
+			}
+			
 		}
 		
 		var toReturn = {};
@@ -39,28 +46,50 @@ Array.prototype.accessify = function() {
 		for (var i = 0; i < methodGroups.length; i++) {
 			//TODO: Do checking for invalid accessor modifiers here
 			if (methodGroups[i][0] == "public") {
-				toReturn[methodGroups[i][1].name] = returnFunction[methodGroups[i][1].name];
+				if (typeof(methodGroups[i][1]) == 'function') {
+					toReturn[methodGroups[i][1].name] = returnFunction[methodGroups[i][1].name];
+				} else if (typeof(methodGroups[i][1]) == 'object') {
+					for (var property in methodGroups[i][1]) {
+						toReturn[property] = methodGroups[i][1][property];
+					};
+				}
 			}
 		}
-		toReturn.that = returnFunction;
+		that = returnFunction;
 		return toReturn;
 	}();
 };
 
 var acTest = [
-	// "private", num,
+	"private", { num: 2, hi: 'Test' },
 	"private", function test() {
 		console.log('test begin called');
 	},
 	"public", function Test() {
 		console.log('Test begin called');
-		console.log(this.that);
-		this.that.test();
+		that.test();
 	},
 	"public", function GetNum() {
-		return num;
+		return that.num;
 	},
 	"public", function SetNum(numToSet) {
-		num = numToSet;
+		that.num = numToSet;
+	},
+	"public", function GetTest() {
+		return that.hi;
 	}
 ].accessify();
+
+// var thisModuleTest = function() {
+// 	var that = this;
+// 	this.test = function() {
+// 		console.log('test being called');
+// 	};
+// 	this.Test = function() {
+// 		console.log('Test being called');
+// 		that.test();
+// 	};
+// 	return {
+// 		Test: this.Test
+// 	}
+// }();
